532dfd0de01cd8f9014e4db0f55bff8739a9b994
Michael Torres <michael.46@live.it>
2025-04-23 01:57:42 +0200

    first commit 1.0

---
+++ counter-vue3/src/components/Counter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const count = ref(0);
+const addingOne = () => {
+  count.value++;
+};
+const resetNum = () => {
+  count.value = 0;
+};
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="addingOne">+1</button>
+  <button @click="resetNum">Reset</button>
+</template>
